c4e100f38b1cf51aa275d3d26475b736d5418e20
Adrianaortiz00 <a0207056@gmail.com>
2024-07-04 11:47:53 +0200

    colors-form

--- hackathon-project/src/components/form/inputCuando/InputCuando.jsx
+++ hackathon-project/src/components/form/inputCuando/InputCuando.jsx
@@ -1,7 +1,7 @@
 const InputCuando = ({ value, onChange }) => {
   return (
     <div className="form-group mb-3">
-      <label htmlFor="Cuando">Cuando</label>
+      <label htmlFor="Cuando">¿Cúando?</label>
       <input
         type="date"
         className="form-control"
--- hackathon-project/src/pages/formPage/FormPage.jsx
+++ hackathon-project/src/pages/formPage/FormPage.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import SelectDonde from '../components/form/SelectDonde';
-import InputCuando from '../components/form/InputCuando';
-import InputAque from '../components/form/InputAque';
+import SelectDonde from '../../components/form/selectDonde/SelectDonde';
+import InputCuando from '../../components/form/inputCuando/InputCuando';
+import InputAque from '../../components/form/inputAque/InputAque';
+import './FormPage.scss'; 
 
 const FormPage = () => {
   const [formData, setFormData] = useState({
@@ -39,7 +40,8 @@ const FormPage = () => {
   }
 
   return (
-    <div className="container ">
+    <div className="background-page">
+    <div className="container">
       <h1 className="text-center">¿Nos vamos?</h1>
       <form onSubmit={handleSubmit}  className='m-5'>
         <SelectDonde value={formData.Donde} onChange={handleDondeChange} />
@@ -47,6 +49,7 @@ const FormPage = () => {
         <InputAque value={formData.Aque} onChange={handleChange} />
         <button type="submit" className="btn btn-secondary">Enviar</button>
       </form>
+    </div>
     </div>
   );
 }
