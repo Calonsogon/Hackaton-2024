477dd00637cb26e2a4d3edb54e4743a8018fc1fb
Carol <calonsogon6@gmail.com>
2024-07-04 14:42:43 +0200

    Finished Map

---
+++ hackathon-project/src/pages/MapPage.jsx
@@ -0,0 +1,20 @@
+// components/MapPage.jsx
+
+import React from 'react';
+import Map from '../components/map/Map';
+
+
+
+const MapPage = () => {
+    return (
+        <div className="map-page">
+            
+            <div className="map-container">
+              <Map />
+            </div>
+        </div>
+    );
+};
+
+export default MapPage;
+
