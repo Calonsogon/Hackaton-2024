d1fef71e256636f99cb8d6ad6f138c0e42731390
Oscar <oscar15-91@hotmail.com>
2024-07-04 15:48:31 +0200

    cambios en Pagina Principal

---
+++ hackathon-project/src/components/lema/Lema.jsx
@@ -0,0 +1,11 @@
+import "./Lema.scss";
+
+function AddLema() {
+  return (
+    <div id="containerLogo">
+      <h2 id="lema">Un viaje seguro</h2>
+    </div>
+  );
+}
+
+export default AddLema;
--- hackathon-project/src/pages/HomePage.jsx
+++ hackathon-project/src/pages/HomePage.jsx
@@ -1,15 +1,22 @@
 import { Link } from "react-router-dom";
 import "./homepage/HomePage.scss";
+import Welcome from "../components/welcome/Welcome";
+import Logo from "../components/logo/Logo";
+import Lema from "../components/lema/Lema";
+import Button from "../components/button/Button";
 
 const HomePage = () => {
   return (
     <div className="background-page">
-      <div className="container mx-auto mt-8">
-        <h2>Bienvenida a nuestro proyecto</h2>
-        <p>Contenido de bienvenida...</p>
-        <Link to="/form" className="btn">
-          Ir al Formulario
-        </Link>
+      <div id="containerwelcome">
+        <Welcome></Welcome>
+        <Logo></Logo>
+      </div>
+      <div id="containerLema">
+        <Lema></Lema>
+      </div>
+      <div id="containerButton">
+        <Button></Button>
       </div>
     </div>
   );
