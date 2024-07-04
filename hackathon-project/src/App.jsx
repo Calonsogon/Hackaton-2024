83041e8ef35c6dff568ec08f47a2d303246b97ef
noemipeteilh <169151883+noemipeteilh@users.noreply.github.com>
2024-07-04 13:04:04 +0200

    changes in index

--- hackathon-project/src/App.jsx
+++ hackathon-project/src/App.jsx
@@ -3,14 +3,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
-import HomePage from './pages/HomePage';
-<<<<<<< HEAD
+import HomePage from './pages/HomePage'; 
 import FormPage from './pages/formPage/FormPage';
-import 'bootstrap/dist/css/bootstrap.min.css'
-=======
-import FormPage from './pages/FormPage';
 import MapPage from './pages/MapPage';
->>>>>>> 0f5a93e0273562b3855efced8d571864ce979c3b
+
 
 const App = () => {
   return (
