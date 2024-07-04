6183c7c11f02a4916bfbbf4001d0c7359ea0043f
Oscar <oscar15-91@hotmail.com>
2024-07-04 17:59:06 +0200

    cambios en boton

--- hackathon-project/src/components/button/Button.jsx
+++ hackathon-project/src/components/button/Button.jsx
@@ -4,12 +4,7 @@ import { Link } from "react-router-dom";
 function AddButtonHome() {
   return (
     <Link to="/Form">
-      <button
-        type="button"
-        id="button"
-        className="btn btn-lg btn-primary"
-        disabled
-      >
+      <button type="button" className="btn btn-secondary">
         Ir al formulario
       </button>
     </Link>
